93104d0e2ccd7a8a200ce437a8cdf77c6dab405e
Claudia Beresford <cberesford@pivotal.io>
2018-05-11 14:54:14 +0100

    flightattendant
    an overengineered way of knowing whether you are logged
    into concourse or not
    useful for tests

---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/concourse/fly/rc"
+)
+
+func main() {
+	var targetName string
+	flag.StringVar(&targetName, "target", "", "check whether you are logged into concourse")
+	flag.Parse()
+
+	if targetName == "" {
+		targetName = os.Getenv("FLY_TARGET")
+		if targetName == "" {
+			fmt.Println("must specify --target")
+			os.Exit(1)
+		}
+	}
+
+	tg := rc.TargetName(targetName)
+
+	targets, err := rc.LoadTargets()
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+
+	if t, ok := targets.Targets[tg]; ok {
+		if t.Token.Value != "" {
+			os.Exit(0)
+		}
+	}
+
+	os.Exit(1)
+}
